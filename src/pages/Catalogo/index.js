3e69e8ae361ed707f0de9769a805eddcd834920c
Rosana Rezende <rezende_rosana@hotmail.com>
2021-01-21 12:04:23 -0300

    corrige renderização de pokemons no catálogo

--- src/pages/Catalogo/index.js
+++ src/pages/Catalogo/index.js
@@ -1,44 +1,58 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import * as S from "./styles";
+import { Typography, Button } from "@material-ui/core";
 
 function Catalogo() {
-    const [pokemons, setPokemons] = useState([]);
+  const [pokemons, setPokemons] = useState([]);
 
-    useEffect(() => {
-        axios.get("https://pokeapi.co/api/v2/pokemon").then(response => 
-        {
-           const result = response.data.results.map(item => {
-               return {
-                   ...item,
-                   type:  axios.get(item.url).then(response => response).then(data =>data)
-               }
-           })
-             
-            setPokemons(result)
-        })
-    }, []);
+  const getPokemons = async () => {
+    const { results } = (
+      await axios.get("https://pokeapi.co/api/v2/pokemon")
+    ).data;
+    const resultFormatted = results?.map(async (item, index) => {
+      const urlInfo = await axios.get(item.url);
+      const { data } = await Promise.resolve(urlInfo);
+      return {
+        name: item.name,
+        id: index + 1,
+        image: data?.sprites.front_default,
+        types: data?.types,
+      };
+    });
+    (async () => {
+      const result = await Promise.all(resultFormatted);
+      setPokemons(result);
+    })();
+  };
 
-    return (
-        <div>
-            <h1>Catálogo</h1>
-            {pokemons?.map((data, index) => {
-                let test 
-                const pokemon = {
-                    name: data.name,
-                    id: index + 1,
-                    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`,
-                    type:  axios.get(data.url).then(response => response).then(data =>data)
-                }
-                    console.log(data        )
-                return (<div key={pokemon.id}>
-                    {pokemon.id}
-                    {pokemon.name}
-                    {data.type}
-                    <img alt={pokemon.name} src={pokemon.image} />
-                </div>)
-            })}
-        </div>
-    )
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  return (
+    <S.PageWrapper>
+      <Typography variant="h2" align="center" gutterBottom>
+        Catálogo
+      </Typography>
+
+      {pokemons?.map((data) => (
+        <S.PaperStyled key={data.id}>
+          <img alt={data.name} src={data.image} />
+          <div className="info">
+            <h2># {data.id}</h2>
+            <h3>{data.name.toUpperCase()}</h3>
+            <p>
+              <strong>Type</strong>: {data.types[0].type.name.toUpperCase()}
+            </p>
+          </div>
+          <Button color="primary" variant="contained">
+            Capturar
+          </Button>
+        </S.PaperStyled>
+      ))}
+    </S.PageWrapper>
+  );
 }
 
-export default Catalogo
+export default Catalogo;
---
+++ src/pages/Catalogo/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components"
+import { Paper } from "@material-ui/core"
+
+export const PageWrapper = styled.div`
+  margin: 2em;
+`
+
+export const PaperStyled = styled(Paper)`
+  display: flex;
+  height: 20vh;
+  margin: 2vh 5vw;
+  padding: 5px 10px;
+  align-items: center;
+  justify-content: space-between;
+
+  img {
+    height: 100%;
+    margin-right: 30px;
+  }
+
+  .info {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  button {
+    padding: 10px 25px;
+  }
+`
